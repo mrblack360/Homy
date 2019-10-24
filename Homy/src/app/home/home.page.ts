61c7ecc80838ce96b056f6b84066b3a31fa8057a
mrblack360 <rmaswi360@outlook.com>
2019-10-24 10:32:28 +0300

    Feat(Home Component): Added bottom tabs

--- Homy/src/app/home/home.page.ts
+++ Homy/src/app/home/home.page.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  bottomTabs = [
+    {
+      tab: 'home',
+      icon: 'home',
+      label: 'Home',
+      url: '',
+      badge: 0
+    },
+    {
+      tab: 'post',
+      icon: 'chatboxes',
+      label: 'Post',
+      url: 'p',
+      badge: 8
+    },
+    {
+      tab: 'platform',
+      icon: 'globe',
+      label: 'Platform',
+      url: 'p',
+      badge: 0
+    },
+    {
+      tab: 'profile',
+      icon: 'person',
+      label: 'Profile',
+      url: 'p',
+      badge: 0
+    }
+  ];
 
   constructor() {}
-
 }
